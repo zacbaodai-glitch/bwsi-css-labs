7ad4d4b392b60bec92924a558900093cb2c6a5ea
zacbaodai-glitch <zacbaodai@gmail.com>
2026-02-10 23:12:26 -0500

    Update lab_1b.py

--- labs/lab_1/lab_1b.py
+++ labs/lab_1/lab_1b.py
@@ -8,7 +8,7 @@ The script asks the user to input the numbers and the operation to be performed,
 and prints the result to the terminal window.
 
 """
-def request_sanitized_number(promt: str) -> float:
+def request_sanitized_number(prompt: str) -> float:
     """funtion to request and sanitize user input for the operation
     
     Returns: 
@@ -20,6 +20,15 @@ def request_sanitized_number(promt: str) -> float:
             return number
         except ValueError:
             print("Invalid input. Please enter a valid number.")
+
+def request_sanitized_operation(prompt: str) -> str:
+    while True:
+        operation = input(prompt).strip().lower()
+        if operation in ["add", "subtract", "multiply", "divide"]:
+            return operation
+        else:
+            print("Invalid input. Please enter add, subtract, multiply, or divide.")
+
 
 def simple_calculator(operation: str, num1: float, num2: float) -> float:
     """
@@ -54,9 +63,9 @@ def main():
     print(f"===== Simple Calculator =====")
 
     # Ask the user for sample input    
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
-    operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
+    num1 = request_sanitized_number("Enter the First number: ")
+    num2 = request_sanitized_number("Enter the second number: ")
+    operation = request_sanitized_operation("Enter the operation (add, subtract, multiply, divide): ")
 
     # Perform the calculation and display the result
     result = simple_calculator(operation, num1, num2)
